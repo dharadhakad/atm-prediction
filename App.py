735e86e2ca0b3c4b17215f52f45d90365079cf56
dharadhakad <85094698+dharadhakad@users.noreply.github.com>
2022-03-21 20:49:38 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,41 @@
+import numpy as np
+import joblib
+import sklearn
+import pandas
+import streamlit as st
+reg = joblib.load('KNN.joblib')
+
+def welcome():
+    return "Welcome"
+
+def pred_yield(weekday, Holiday, trans_date_set, trans_month, trans_year, osmia,prevweek_mean):
+
+    prediction = reg.predict([[weekday, Holiday, trans_date_set, trans_month, trans_year, osmia,prevweek_mean]])
+    print('prediction')
+    return prediction
+
+
+def main():
+
+    st.title('ATM Amount Prediction')
+
+    Date = st.text_input('Date(DD/MM/YYYY)','Type Here')
+    weekday = st.text_input('weekday(in number i.e. 1 for sunday)','Type Here')
+    Holiday = st.text_input('Holiday (H for holiday, W for working day)','Type Here')
+    trans_date_set = st.text_input('DATE (DD)','Type Here')
+    trans_month = st.text_input('MONTH','Type Here')
+    trans_year = st.text_input('YEAR','Type Here')
+    prevweek_mean = st.text_input('Previous Week Average','Type Here')
+    pred=""
+    result=0
+    row=1
+    if st.button("Predict"):
+        result=pred_yield(weekday, Holiday, trans_date_set, trans_month, trans_year, osmia,prevweek_mean)
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Built by Dhara Dhakad")
+
+
+if __name__ == '__main__':
+
+    main()
